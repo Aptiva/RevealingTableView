2e2d42ab6216e036fe636760264c7027dd089cf8
Aptiva <aptiva.app@gmail.com>
2012-06-15 05:11:08 +0900

    Initial Commit

---
+++ RevealingTableView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RevealingTableView
+//
+//  Created by aptiva on 12-6-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RevealingTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RevealingTableView
+//
+//  Created by aptiva on 12-6-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
